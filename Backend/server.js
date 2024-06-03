677501217740622df7a8655892f7e33aa522daef
Hunter Wilson <huntertheguy123@gmail.com>
2024-06-02 21:57:34 -0500

    Update database connection for production

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const { Sequelize } = require('sequelize');
-const { OliveOil, Balsamic } = require('./models'); // Ensure Balsamic is also imported
+const { OliveOil, Balsamic } = require('./db/models'); // Ensure Balsamic is also imported
 
 const app = express();
 const port = process.env.PORT || 3001;

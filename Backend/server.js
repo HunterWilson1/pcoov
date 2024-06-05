b280db6d608fc2fdfdb37b11f5fe4044e63d384e
Hunter Wilson <huntertheguy123@gmail.com>
2024-06-04 19:49:27 -0500

    Setup for Heroku deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,16 @@
-// Backend/server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const connectDB = require('./db/connection');
 const { OliveOil, Balsamic } = require('./db/models');
-require('dotenv').config(); // Ensure this is at the top
+require('dotenv').config();
 
-// Log the environment variable to verify it's being loaded
-console.log('MONGO_URI:', process.env.MONGO_URI);
-
-// Initialize express app
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
 
-// Connect to MongoDB
 connectDB();
 
-// Routes for Olive Oils
 app.get('/api/olive_oils', async (req, res) => {
   try {
     const oliveOils = await OliveOil.find({});
@@ -38,7 +30,6 @@ app.get('/api/olive_oils/:tags', async (req, res) => {
   }
 });
 
-// Routes for Balsamics
 app.get('/api/balsamics', async (req, res) => {
   try {
     const balsamics = await Balsamic.find({});
@@ -59,12 +50,14 @@ app.get('/api/balsamics/:tags', async (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// Start the server
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

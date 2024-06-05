5481aa422376371eecc33a41a0865cdc19d63504
Hunter Wilson <huntertheguy123@gmail.com>
2024-06-04 19:57:21 -0500

    Fix package.json dependencies

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -1,18 +1,15 @@
-// Backend/db/connection.js
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI;
-    console.log('Connecting to MongoDB with URI:', uri); // Log the URI to verify
-    const conn = await mongoose.connect(uri, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
     process.exit(1);
   }
 };
